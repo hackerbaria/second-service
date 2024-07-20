dd19e502ea533a3c099c469137cc2519577b9330
Thong Nguyen <thong.nguyen@unifiedpost.com>
2024-07-20 21:44:52 +0700

    hbyh

---
+++ src/main/java/com/javainuse/controller/OpenAPIConfiguration.java
@@ -0,0 +1,32 @@
+package com.javainuse.controller;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfiguration {
+
+    @Bean
+    public OpenAPI defineOpenApi() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8082");
+        server.setDescription("Second Service");
+
+        Contact myContact = new Contact();
+        myContact.setName("Jane Doe");
+        myContact.setEmail("your.email@gmail.com");
+
+        Info information = new Info()
+                .title("Consumer Management System API")
+                .version("1.0")
+                .description("This API exposes endpoints to manage Consumers.")
+                .contact(myContact);
+        return new OpenAPI().info(information).servers(List.of(server));
+    }
+}
